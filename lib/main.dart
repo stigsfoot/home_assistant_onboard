12efb13239ef2f6d8974fdb53befbdb6589ed651
Noble Ackerson <noble.ackerson@gmail.com>
2020-09-13 22:56:26 -0400

    feat: added google auth stub

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:home_assistant_onboard/screens/reminders.dart';
-import 'package:home_assistant_onboard/screens/about.dart';
-import 'package:home_assistant_onboard/screens/onboarding.dart';
 import 'package:home_assistant_onboard/screens/screens.dart';
+import 'package:provider/provider.dart';
+import 'package:home_assistant_onboard/services/services.dart';
 
 // fewer lines
 // import 'screens/screens.dart';
@@ -17,25 +18,39 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(
+      providers: [
+        StreamProvider<FirebaseUser>.value(value: AuthService().user),
+      ],
+      child: MaterialApp(
         // Firebase Analytics
         navigatorObservers: [
           FirebaseAnalyticsObserver(analytics: FirebaseAnalytics()),
         ],
 
+        //Routes
         routes: {
           '/': (context) => LoginScreen(),
-          '/about': (context) => AboutScreen(),
-          '/onboarding': (context) => OnboardingScreen(),
+          //'/about': (context) => AboutScreen(),
+          //'/onboarding': (context) => OnboardingScreen(),
           '/profile': (context) => ProfileScreen(),
           '/reminders': (context) => RemindersScreen(),
         },
 
         // Theme
+        // Theme
         theme: ThemeData(
-            // your customizations here
-            brightness: Brightness.dark,
+          fontFamily: 'Nunito',
+          bottomAppBarTheme: BottomAppBarTheme(
+            color: Colors.black87,
           ),
-      );
+          brightness: Brightness.dark,
+          textTheme: TextTheme(
+            button: TextStyle(letterSpacing: 1.5, fontWeight: FontWeight.bold),
+          ),
+          buttonTheme: ButtonThemeData(),
+        ),
+      ),
+    );
   }
 }

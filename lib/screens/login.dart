63fcb7bf567dd473247347083c06595ee3da2af7
Noble Ackerson <noble.ackerson@gmail.com>
2020-09-14 00:13:06 -0400

    feat: added login flow/logic

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,18 +1,124 @@
 import 'package:flutter/material.dart';
-import 'package:home_assistant_onboard/shared/navigation.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../services/services.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  AuthService auth = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+    auth.getUser.then(
+      (user) {
+        if (user != null) {
+          Navigator.pushReplacementNamed(context, '/onboarding');
+        }
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Assistant Login'),
-        backgroundColor: Colors.black,
+      body: Container(
+        padding: EdgeInsets.all(30),
+        decoration: BoxDecoration(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            FlutterLogo(
+              size: 150,
+            ),
+            Text(
+              'Welcome',
+              style: Theme.of(context).textTheme.headline5,
+              textAlign: TextAlign.center,
+            ),
+            Text('Home Assistant remembers so you do not have to.'),
+            LoginButton(
+              text: 'LOGIN WITH GOOGLE',
+              icon: FontAwesomeIcons.google,
+              color: Colors.black45,
+              loginMethod: auth.googleSignIn,
+            ),
+
+            // Privacy settings
+            PrivacySettingsButton(
+              text: 'Privacy Settings',
+              icon: FontAwesomeIcons.lock,
+              color: Colors.green[300],
+            ),
+            PrivacySettingsButton(),
+            LoginButton(text: 'Access without logging in'),
+          ],
+        ),
       ),
-      body: Center(
-        child: Text('Login stub'),
+    );
+  }
+}
+
+// Login widget we can reuse
+class LoginButton extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  final String text;
+  final Function loginMethod;
+
+  const LoginButton(
+      {Key key, this.text, this.icon, this.color, this.loginMethod})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 10),
+      child: FlatButton.icon(
+        padding: EdgeInsets.all(30),
+        icon: Icon(icon, color: Colors.white),
+        color: color,
+        onPressed: () async {
+          var user = await loginMethod();
+          if (user != null) {
+            Navigator.pushReplacementNamed(context, '/onboarding');
+          }
+        },
+        label: Expanded(
+          child: Text('$text', textAlign: TextAlign.center),
+        ),
       ),
-      bottomNavigationBar: AppBottomNav(),
+    );
+  }
+}
+
+// Privacy Settings widget we can reuse
+class PrivacySettingsButton extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  final String text;
+
+  const PrivacySettingsButton({Key key, this.text, this.icon, this.color})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 10),
+      child: FlatButton.icon(
+        padding: EdgeInsets.all(30),
+        icon: Icon(icon, color: Colors.white),
+        color: color,
+        onPressed: () {
+          //TODO: insert privacy overlay
+        },
+        label: Expanded(
+          child: Text('$text', textAlign: TextAlign.center),
+        ),
+      ),
     );
   }
 }
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,18 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:home_assistant_onboard/shared/navigation.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../services/services.dart';
+import 'package:provider/provider.dart';
 
 class ProfileScreen extends StatelessWidget {
+  final AuthService auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('User Profile'),
-        backgroundColor: Colors.black,
-      ),
-      body: Center(
-        child: Text('Profile stub'),
-      ),
-      bottomNavigationBar: AppBottomNav(),
-    );
+    FirebaseUser user = Provider.of<FirebaseUser>(context);
+
+    if (user != null) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          title: Text(user.displayName ?? 'Guest'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              if (user.photoUrl != null)
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: EdgeInsets.only(top: 50),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: NetworkImage(user.photoUrl),
+                    ),
+                  ),
+                ),
+              Text(user.email ?? '',
+                  style: Theme.of(context).textTheme.headline5),
+              Spacer(),
+              FlatButton(
+                  child: Text('logout'),
+                  color: Colors.red,
+                  onPressed: () async {
+                    await auth.signOut();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/', (route) => false);
+                  }),
+              Spacer()
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Text('not logged in...');
+    }
   }
 }
